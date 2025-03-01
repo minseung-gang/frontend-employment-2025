e5103dd8993e6b2be1545aa469ee213c09932ce0
minseung-gang <strause1@nate.com>
2025-03-01 19:24:00 +0900

    feat: 포켓몬 리스트 페이지 추가

---
+++ src/app/(pokemon)/pokemons/page.tsx
@@ -0,0 +1,10 @@
+import PokemonList from '@/components/pokemon/PokemonList';
+import React from 'react';
+
+export default function PokemonsPage() {
+  return (
+    <div className="flex p-10 justify-center">
+      <PokemonList />
+    </div>
+  );
+}
